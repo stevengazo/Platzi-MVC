49ac754eed7755ab1ddff832e2c09cc6f07c5a45
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-05 21:05:37 -0600

    Clase 14: Vistas Parciales en Asignatura

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,20 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using platzi_curso_csharp_Etapa7.Models;
 using System;
+using System.Collections.Generic;
 
 namespace platzi_curso_csharp_Etapa7.Controllers
 {
-    public class AsignaturaController: Controller
+    public class AsignaturaController : Controller
     {
-        public IActionResult Index()
-        {
-            var asignatura = new Asignatura(){
-                UniqueId= Guid.NewGuid().ToString(),
-                Nombre= "Programación"
-            };
-            ViewBag.cosaDinamica= "Prueba";
-            ViewBag.Fecha = DateTime.Now;
-            return View(asignatura);
+
+        public IActionResult Index(){
+            return View(new Asignatura{Nombre="Programación",     UniqueId= Guid.NewGuid().ToString()} );
         }
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                new Asignatura{Nombre="Matematicas",        UniqueId= Guid.NewGuid().ToString()},
+                new Asignatura{Nombre="Educación Fisica",   UniqueId= Guid.NewGuid().ToString()},
+                new Asignatura{Nombre="Castellano",         UniqueId= Guid.NewGuid().ToString()},
+                new Asignatura{Nombre="Programación",         UniqueId= Guid.NewGuid().ToString()},
+                new Asignatura{Nombre="Ciencias Naturales", UniqueId= Guid.NewGuid().ToString()}
+            };
+        
+            ViewBag.cosaDinamica = "Prueba";
+            ViewBag.Fecha = DateTime.Now;
+            return View("MultiAsignatura",listaAsignaturas);
+        }
+
     }
 }
