dac8dad756fdcbc0966298cbc18d72eeb7a8aa1a
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-08 09:49:37 -0600

    Clase 16: actualiación de bootstrap y tablas

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -11,11 +11,11 @@ namespace platzi_curso_csharp_Etapa7.Controllers
 
         public IActionResult Index(){
             ViewBag.Fecha= DateTime.Now;
-            return View(new Alumno{Nombre="Pepe ",     UniqueId= Guid.NewGuid().ToString()} );
+            return View(new Alumno{Nombre="Pepe ", UniqueId= Guid.NewGuid().ToString()} );
         }
         public IActionResult MultiAlumno()
         {
-            List<Alumno> listaAlumno = GenerarAlumnosAlAzar();        
+            List<Alumno> listaAlumno = GenerarAlumnosAlAzar();  
             ViewBag.cosaDinamica = "Prueba";
             ViewBag.Fecha = DateTime.Now;
             return View("MultiAlumno",listaAlumno);
@@ -30,7 +30,8 @@ namespace platzi_curso_csharp_Etapa7.Controllers
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno {  Nombre = $"{n1} {n2} {a1}",
+                                                    UniqueId = Guid.NewGuid().ToString() };
 
             return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
         }
