a2beef8cf808f5a358aa1f5db797b46346f99568
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-09 11:03:39 -0600

    Reto 1: Adición de evaluaciones

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -36,9 +36,7 @@ namespace Platzi_MVC_CSharp.Models
             modelBuilder.Entity<Escuela>().HasData(escuela);
 
             //Cargar Cursos Escuela
-            var cursos =CargarCursos(escuela);
-
-
+            var cursos = CargarCursos(escuela);
 
             //x cada curso  cargar: asignaturas 
             var asignaturas = CargarAsignaturas(cursos);
@@ -46,11 +44,17 @@ namespace Platzi_MVC_CSharp.Models
             //x cada curso cargar: alumnos
             var alumnos = CargarAlumnos(cursos);
 
+            //x cada alumno en una asignatura cargar una evaluación
+            var evaluaciones = CargarEvaluaciones(alumnos, asignaturas);
+        
+
+
+            // SIEMBRA DE DATOS
             modelBuilder.Entity<Escuela>().HasData(escuela);
             modelBuilder.Entity<Curso>().HasData(cursos.ToArray());
             modelBuilder.Entity<Asignatura>().HasData(asignaturas.ToArray());
             modelBuilder.Entity<Alumno>().HasData(alumnos.ToArray());
-            
+            modelBuilder.Entity<Evaluación>().HasData(evaluaciones.ToArray());
 
             static List<Curso> CargarCursos(Escuela escuela)
             {
@@ -64,12 +68,34 @@ namespace Platzi_MVC_CSharp.Models
             }
         }
 
+        private List<Evaluación> CargarEvaluaciones(List<Alumno> alumnos, List<Asignatura> asignaturas)
+        {
+            Random rnd = new Random();
+            var tmpList = new List<Evaluación>();
+            foreach (var alumno in alumnos)
+            {
+                foreach (var asignatura in asignaturas)
+                {
+                    var temp = new Evaluación()
+                    {
+                        Nombre = $"Eval: {alumno.Nombre} - {asignatura.Nombre}",
+                        Id = Guid.NewGuid().ToString(),
+                        AlumnoId = alumno.Id,
+                        AsignaturaId = asignatura.Id,
+                        Nota = rnd.Next(50, 80)
+                    };
+                    tmpList.Add(temp);
+                }
+            }
+            return tmpList;
+        }
+
         private List<Asignatura> CargarAsignaturas(List<Curso> Cursos)
         {
             var ListaCompleta = new List<Asignatura>();
             foreach (var Curso in Cursos)
             {
-                var tmpList= new List<Asignatura>{
+                var tmpList = new List<Asignatura>{
                     new Asignatura{Id= Guid.NewGuid().ToString(),CursoId= Curso.Id, Nombre= "Matematicas"},
                     new Asignatura{Id= Guid.NewGuid().ToString(),CursoId= Curso.Id, Nombre= "Español"},
                     new Asignatura{Id= Guid.NewGuid().ToString(),CursoId= Curso.Id, Nombre= "Ciencias"},
@@ -95,18 +121,20 @@ namespace Platzi_MVC_CSharp.Models
                                {
                                    Nombre = $"{n1} {n2} {a1}",
                                    Id = Guid.NewGuid().ToString(),
-                                    CursoId= curso.Id
+                                   CursoId = curso.Id
                                };
 
             return listaAlumnos.OrderBy((al) => al.Id).Take(cantidad).ToList();
         }
 
-        private List<Alumno> CargarAlumnos(List<Curso> cursos){
-            var listaAlumnos= new List<Alumno>();
+        private List<Alumno> CargarAlumnos(List<Curso> cursos)
+        {
+            var listaAlumnos = new List<Alumno>();
             Random rnd = new Random();
-            foreach (var _curso in cursos){
-                int cantRandom= rnd.Next(5,20);
-                var tmpList = GenerarAlumnosAlAzar(cantRandom,_curso);
+            foreach (var _curso in cursos)
+            {
+                int cantRandom = rnd.Next(5, 20);
+                var tmpList = GenerarAlumnosAlAzar(cantRandom, _curso);
                 listaAlumnos.AddRange(tmpList);
 
             }
