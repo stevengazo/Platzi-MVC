216a54675b8b9d3817c7e7949cf349b407b9fad9
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-09 11:52:16 -0600

    Clase 24: Ajustes de enrutamiento en Asignatura

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -8,24 +8,37 @@ namespace Platzi_MVC_CSharp.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index(string AsignaturaId){
-            var asignatura = from asig in _context.Asignaturas
-                            where asig.Id == AsignaturaId
-                            select asig;
-            return View(asignatura.SingleOrDefault());
+        [Route("Asignatura/Index")]
+
+        [Route("Asignatura/Index/{asignaturaId}")]
+        public IActionResult Index(string asignaturaId)
+        {
+            if (!string.IsNullOrEmpty(asignaturaId))
+            {
+                var asignatura = from asig in _context.Asignaturas
+                                 where asig.Id == asignaturaId
+                                 select asig;
+                return View(asignatura.SingleOrDefault());
+            }
+            else
+            {
+return View("MultiAsignatura", this._context.Asignaturas);
+            }
+
         }
         public IActionResult MultiAsignatura()
-        {        
+        {
             ViewBag.cosaDinamica = "Prueba";
             ViewBag.Fecha = DateTime.Now;
-            return View("MultiAsignatura",this._context.Asignaturas);
+            return View("MultiAsignatura", this._context.Asignaturas);
         }
         //constructor de la clase y especificación del DBContext
         private EscuelaContext _context;
-        public AsignaturaController(EscuelaContext context){
+        public AsignaturaController(EscuelaContext context)
+        {
             this._context = context;
         }
-        
+
 
     }
 }
